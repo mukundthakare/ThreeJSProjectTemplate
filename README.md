0b23170d84579693f940b797dca8f1b8afb2ff08
thakare.mukund <thakare.mukund@cct-inc.co.jp>
2024-08-04 16:36:17 +0530

    Initial commit for three.js application

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ThreeJSProjectTemplate
-Sample Template application to create three.js application
+This Repository contains the sample template for creating the three.js application.
+
+How to use the template ?
+    1) Clone the repository or download the code.
+    2) Do npm install - to install all the dependencies
+    3) Do npm run dev to run this project.
+    4) Open localhost:8080 in your browser to run the three. js application.
+
+How to modify the application ?
+    1) You can start adding three.js code using typescript in the client.ts file.
---
+++ src/client/client.ts
@@ -0,0 +1,34 @@
+
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 20;
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+new OrbitControls(camera, renderer.domElement);
+
+const axisControl = new THREE.AxesHelper(10);
+scene.add(axisControl);
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  render()
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  render();
+}
+
+function render() {
+  renderer.render(scene, camera)
+}
+
+animate()
