46bd3208fe836df07665fa75457b3be2dfd1801d
thakare.mukund <thakare.mukund@cct-inc.co.jp>
2024-08-17 10:45:19 +0530

    - Added code for animation of multiple geometry

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,45 +1,54 @@
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
+// container
+let objects: THREE.Mesh[] = [];
+
 //1
 const scene = new THREE.Scene();
 
 //2
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 70;
-camera.position.x = 70;
-camera.position.y = 70;
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.setZ(70);
 //3
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 //4
-new OrbitControls(camera, renderer.domElement);
+let orbitControls = new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.BufferGeometry();
+//5
+const axes = new THREE.AxesHelper(100);
+scene.add(axes);
 
-// create a simple square shape. We duplicate the top left and bottom right
-// vertices because each vertex needs to appear once per triangle.
-const vertices = new Float32Array([
-  -1.0, -1.0, 1.0, // v0
-  1.0, -1.0, 1.0, // v1
-  1.0, 1.0, 1.0, // v2
+//6
+const coneGeometry = new THREE.ConeGeometry(2, 5, 32, 32);
+const boxGeometry = new THREE.BoxGeometry(5, 5, 5);
+const capsuleGeometry = new THREE.CapsuleGeometry(1, 2, 32, 32);
+const hedrongeometry = new THREE.DodecahedronGeometry(1, 1);
+const circleGeometry = new THREE.CircleGeometry(5, 32);
 
-  1.0, 1.0, 1.0, // v3
-  -1.0, 1.0, 1.0, // v4
-  -1.0, -1.0, 1.0  // v5
-]);
-
-geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-//axes helper
-const axisControl = new THREE.AxesHelper(100);
-scene.add(axisControl);
+//7 create objects
+for (let i = 0; i < 1000; i++) {
+  let material = new THREE.MeshBasicMaterial({ color: 0xffff00 * Math.random() })
+  if (i < 200) {
+    createMeshFromGeometryAndMat(coneGeometry, material);
+  }
+  else if (i >= 200 && i < 400) {
+    createMeshFromGeometryAndMat(boxGeometry, material);
+  }
+  else if (i >= 400 && i < 600) {
+    createMeshFromGeometryAndMat(capsuleGeometry, material);
+  }
+  else if (i >= 600 && i < 800) {
+    createMeshFromGeometryAndMat(hedrongeometry, material);
+  } else {
+    createMeshFromGeometryAndMat(circleGeometry, material)
+  }
+}
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -55,7 +64,25 @@ function animate() {
 }
 
 function render() {
+  scene.rotateX(0.01);
+  scene.rotateY(0.01);
+  scene.rotateZ(0.01);
   renderer.render(scene, camera)
+  for (let i = 0; i < 1000; i++) {
+    //   objects[i].rotateX(0.1);
+    //   objects[i].rotateY(0.1);
+    //   objects[i].rotateZ(0.1);
+    objects[i].scale.set(THREE.MathUtils.randFloatSpread(2), THREE.MathUtils.randFloatSpread(2), THREE.MathUtils.randFloatSpread(2))
+  }
+}
+
+function createMeshFromGeometryAndMat(geometry: THREE.BufferGeometry, material: THREE.MeshBasicMaterial) {
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.setX(THREE.MathUtils.randFloatSpread(100));
+  mesh.position.setY(THREE.MathUtils.randFloatSpread(100));
+  mesh.position.setZ(THREE.MathUtils.randFloatSpread(100));
+  scene.add(mesh);
+  objects.push(mesh);
 }
 
 animate()
